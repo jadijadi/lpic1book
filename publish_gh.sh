ec5135f2dbc03ac8621eaf3b802f1b5d58862090
Jadi <jadijadi@gmail.com>
2024-09-05 19:44:02 +0330

    adding the PDF and one page template

--- publish_gh.sh
+++ publish_gh.sh
@@ -3,3 +3,6 @@
 make html
 ghp-import output
 git push origin gh-pages
+
+
+# for ebook, replace <iframe .*/iframe> with <img src="/images/videoplaceholder.jpg" width=650px > on lpic1_book.html in output and print to PDF
